69058eaae8ad5cf7d8ad275546ff771e108aff56
Muhamad Yasir Noval <myasirnoval@gmail.com>
2025-11-30 17:03:02 +0700

    menambahkan constraint fk untuk jadwal dengan dokter

--- SistemReservasiKlinik/src/main/resources/id/ac/ifunjani/sistemreservasiklinik/db/jadwal.sql
+++ SistemReservasiKlinik/src/main/resources/id/ac/ifunjani/sistemreservasiklinik/db/jadwal.sql
@@ -5,5 +5,7 @@ CREATE table jadwal
     hari        ENUM('SENIN', 'SELASA', 'RABU', 'KAMIS', 'JUMAT', 'SABTU') NOT NULL,
     jam_mulai   TIME       NOT NULL,
     jam_selesai TIME       NOT NULL,
-    id_dokter   CHAR(10)   NOT NULL
+    id_dokter   CHAR(10)   NOT NULL,
+    PRIMARY KEY (id_jadwal),
+    CONSTRAINT fk_jadwal_dokter FOREIGN KEY (id_dokter) REFERENCES dokter (id_dokter) ON DELETE RESTRICT
 ) ENGINE = InnoDB;
