dfdb4dabc801108f9b452b5692109a527ef688b5
MYusronN <hawkinsdamien007@gmail.com>
2025-11-30 17:10:04 +0700

    Memperbaiki font di db pasien supaya Upper case

--- SistemReservasiKlinik/src/main/resources/id/ac/ifunjani/sistemreservasiklinik/db/pasien.sql
+++ SistemReservasiKlinik/src/main/resources/id/ac/ifunjani/sistemreservasiklinik/db/pasien.sql
@@ -1,11 +1,11 @@
 CREATE TABLE pasien
 (
-    id_pasien char(16) NOT NULL,
-    nama_lengkap varchar(100) NOT NULL,
+    id_pasien CHAR(16) NOT NULL,
+    nama_lengkap VARCHAR(100) NOT NULL,
     alamat TEXT,
     tanggal_lahir DATE,
     no_telepon VARCHAR(13) NOT NULL,
-    password varchar(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
 
     PRIMARY KEY (id_pasien)
 )
