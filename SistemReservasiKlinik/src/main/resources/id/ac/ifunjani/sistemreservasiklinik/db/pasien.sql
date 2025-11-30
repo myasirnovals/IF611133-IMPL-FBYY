847ac20ca1ac046930328d8ad68be90c7533c5e3
MYusronN <hawkinsdamien007@gmail.com>
2025-11-30 17:09:14 +0700

    Menambahkan db pasien

---
+++ SistemReservasiKlinik/src/main/resources/id/ac/ifunjani/sistemreservasiklinik/db/pasien.sql
@@ -0,0 +1,11 @@
+CREATE TABLE pasien
+(
+    id_pasien char(16) NOT NULL,
+    nama_lengkap varchar(100) NOT NULL,
+    alamat TEXT,
+    tanggal_lahir DATE,
+    no_telepon VARCHAR(13) NOT NULL,
+    password varchar(255) NOT NULL,
+
+    PRIMARY KEY (id_pasien)
+)
