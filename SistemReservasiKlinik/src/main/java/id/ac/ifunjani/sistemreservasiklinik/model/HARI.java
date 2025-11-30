307099a6f5249e6ebab653b9d3958b5376005c8f
Muhamad Yasir Noval <myasirnoval@gmail.com>
2025-11-30 16:00:46 +0700

    membuat model jadwal

---
+++ SistemReservasiKlinik/src/main/java/id/ac/ifunjani/sistemreservasiklinik/model/HARI.java
@@ -0,0 +1,5 @@
+package id.ac.ifunjani.sistemreservasiklinik.model;
+
+public enum HARI {
+    SENIN, SELASA, RABU, KAMIS, JUMAT, SABTU
+}
---
+++ SistemReservasiKlinik/src/main/java/id/ac/ifunjani/sistemreservasiklinik/model/Jadwal.java
@@ -0,0 +1,12 @@
+package id.ac.ifunjani.sistemreservasiklinik.model;
+
+import java.util.Date;
+
+public class Jadwal {
+    private String idJadwal;
+    private Date tanggal;
+    private HARI hari;
+    private String jamMulai;
+    private String jamSelesai;
+    private String idDokter;
+}
