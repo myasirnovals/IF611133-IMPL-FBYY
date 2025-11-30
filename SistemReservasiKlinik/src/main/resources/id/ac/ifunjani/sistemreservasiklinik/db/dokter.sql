578e440ffb7934ff8479d44f51fe7a2371f55b0a
Muhamad Yasir Noval <myasirnoval@gmail.com>
2025-11-30 16:58:03 +0700

    menambahkan tabel dokter

---
+++ SistemReservasiKlinik/src/main/resources/id/ac/ifunjani/sistemreservasiklinik/db/dokter.sql
@@ -0,0 +1,7 @@
+CREATE TABLE dokter
+(
+    id_dokter CHAR(10) NOT NULL,
+    nama_dokter VARCHAR(100) NOT NULL,
+    spesialis VARCHAR(50) NOT NULL,
+    no_telp VARCHAR(13) NOT NULL
+) ENGINE = InnoDB;
