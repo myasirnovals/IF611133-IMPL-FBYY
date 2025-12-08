ac76dff50d2a4b27ecb875ee034fffcee1bc0643
MYusronN <hawkinsdamien007@gmail.com>
2025-12-09 05:56:25 +0700

    Menambahkan method getter dan setter

--- SistemReservasiKlinik/src/main/java/id/ac/ifunjani/sistemreservasiklinik/model/Pasien.java
+++ SistemReservasiKlinik/src/main/java/id/ac/ifunjani/sistemreservasiklinik/model/Pasien.java
@@ -9,4 +9,52 @@ public class Pasien {
     private LocalDate tanggalLahir;
     private String noTelepon;
     private String password;
+
+    public String getIdPasien() {
+        return idPasien;
+    }
+
+    public String getNamaLengkap() {
+        return namaLengkap;
+    }
+
+    public String getAlamat() {
+        return alamat;
+    }
+
+    public LocalDate getTanggalLahir() {
+        return tanggalLahir;
+    }
+
+    public String getNoTelepon() {
+        return noTelepon;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setIdPasien(String idPasien) {
+        this.idPasien = idPasien;
+    }
+
+    public void setNamaLengkap(String namaLengkap) {
+        this.namaLengkap = namaLengkap;
+    }
+
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
+
+    public void setTanggalLahir(LocalDate tanggalLahir) {
+        this.tanggalLahir = tanggalLahir;
+    }
+
+    public void setNoTelepon(String noTelepon) {
+        this.noTelepon = noTelepon;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
