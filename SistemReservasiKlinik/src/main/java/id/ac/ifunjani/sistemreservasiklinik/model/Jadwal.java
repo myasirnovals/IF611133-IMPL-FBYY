6f2e11439dd12ae53752a3cc8d7c4b0bf1e837b4
Muhamad Yasir Noval <myasirnoval@gmail.com>
2025-12-14 16:55:26 +0700

    mengubah value attribute Date dan Time menjadi LocalTime dan LocalDate, serta menambahkan method dan constructor

--- SistemReservasiKlinik/src/main/java/id/ac/ifunjani/sistemreservasiklinik/model/Jadwal.java
+++ SistemReservasiKlinik/src/main/java/id/ac/ifunjani/sistemreservasiklinik/model/Jadwal.java
@@ -1,12 +1,73 @@
 package id.ac.ifunjani.sistemreservasiklinik.model;
 
-import java.util.Date;
+import java.time.LocalDate;
+import java.time.LocalTime;
 
 public class Jadwal {
     private String idJadwal;
-    private Date tanggal;
-    private HARI hari;
-    private String jamMulai;
-    private String jamSelesai;
+    private LocalDate tanggal;
+    private String hari;
+    private LocalTime jamMulai;
+    private LocalTime jamSelesai;
     private String idDokter;
+
+    public Jadwal() {
+    }
+
+    public Jadwal(String idJadwal, LocalDate tanggal, String hari, LocalTime jamMulai, LocalTime jamSelesai, String idDokter) {
+        this.idJadwal = idJadwal;
+        this.tanggal = tanggal;
+        this.hari = hari;
+        this.jamMulai = jamMulai;
+        this.jamSelesai = jamSelesai;
+        this.idDokter = idDokter;
+    }
+
+    public String getIdJadwal() {
+        return idJadwal;
+    }
+
+    public void setIdJadwal(String idJadwal) {
+        this.idJadwal = idJadwal;
+    }
+
+    public LocalDate getTanggal() {
+        return tanggal;
+    }
+
+    public void setTanggal(LocalDate tanggal) {
+        this.tanggal = tanggal;
+    }
+
+    public String getHari() {
+        return hari;
+    }
+
+    public void setHari(String hari) {
+        this.hari = hari;
+    }
+
+    public LocalTime getJamMulai() {
+        return jamMulai;
+    }
+
+    public void setJamMulai(LocalTime jamMulai) {
+        this.jamMulai = jamMulai;
+    }
+
+    public LocalTime getJamSelesai() {
+        return jamSelesai;
+    }
+
+    public void setJamSelesai(LocalTime jamSelesai) {
+        this.jamSelesai = jamSelesai;
+    }
+
+    public String getIdDokter() {
+        return idDokter;
+    }
+
+    public void setIdDokter(String idDokter) {
+        this.idDokter = idDokter;
+    }
 }
