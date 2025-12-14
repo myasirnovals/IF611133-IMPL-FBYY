f738b95dac2dd3454b0072ef86b21aa6e8d93afd
Juwew1 <fauzyjunior53@gmail.com>
2025-12-14 11:28:44 +0700

    Memperbaiki code Staff

--- SistemReservasiKlinik/src/main/java/id/ac/ifunjani/sistemreservasiklinik/model/Staff.java
+++ SistemReservasiKlinik/src/main/java/id/ac/ifunjani/sistemreservasiklinik/model/Staff.java
@@ -2,60 +2,22 @@ package id.ac.ifunjani.sistemreservasiklinik.model;
 
 public class Staff {
 
-    private Long idStaff;
+    private String idStaff;
     private String nama;
-    private String jabatan;
-    private String alamat;
-    private String nomorTelepon;
+    private String password;
 
-    public Staff() {
-    }
-
-    public Staff(Long idStaff, String nama, String jabatan, String alamat, String nomorTelepon) {
+    public Staff(String idStaff, String nama, String password) {
         this.idStaff = idStaff;
         this.nama = nama;
-        this.jabatan = jabatan;
-        this.alamat = alamat;
-        this.nomorTelepon = nomorTelepon;
+        this.password = password;
     }
 
-    public Long getIdStaff() {
-        return idStaff;
-    }
+    public Staff() {}
 
-    public void setIdStaff(Long idStaff) {
-        this.idStaff = idStaff;
-    }
-
-    public String getNama() {
-        return nama;
-    }
-
-    public void setNama(String nama) {
-        this.nama = nama;
-    }
-
-    public String getJabatan() {
-        return jabatan;
-    }
-
-    public void setJabatan(String jabatan) {
-        this.jabatan = jabatan;
-    }
-
-    public String getAlamat() {
-        return alamat;
-    }
-
-    public void setAlamat(String alamat) {
-        this.alamat = alamat;
-    }
-
-    public String getNomorTelepon() {
-        return nomorTelepon;
-    }
-
-    public void setNomorTelepon(String nomorTelepon) {
-        this.nomorTelepon = nomorTelepon;
-    }
+    public String getIdStaff() { return idStaff; }
+    public void setIdStaff(String idStaff) { this.idStaff = idStaff; }
+    public String getNama() { return nama; }
+    public void setNama(String nama) { this.nama = nama; }
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
 }
