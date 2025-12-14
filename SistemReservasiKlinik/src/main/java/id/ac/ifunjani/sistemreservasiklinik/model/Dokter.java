04016b9739bcef8dbdb747313eecb36a3cea18a7
Muhamad Yasir Noval <myasirnoval@gmail.com>
2025-12-14 16:52:33 +0700

    menambahkan attribute dan method pada kelas dokter

--- SistemReservasiKlinik/src/main/java/id/ac/ifunjani/sistemreservasiklinik/model/Dokter.java
+++ SistemReservasiKlinik/src/main/java/id/ac/ifunjani/sistemreservasiklinik/model/Dokter.java
@@ -5,4 +5,56 @@ public class Dokter {
     private String namaDokter;
     private String spesialis;
     private String noTelp;
+    private String password;
+
+    public Dokter() {
+    }
+
+    public Dokter(String idDokter, String namaDokter, String spesialis, String noTelp, String password) {
+        this.idDokter = idDokter;
+        this.namaDokter = namaDokter;
+        this.spesialis = spesialis;
+        this.noTelp = noTelp;
+        this.password = password;
+    }
+
+    public String getIdDokter() {
+        return idDokter;
+    }
+
+    public void setIdDokter(String idDokter) {
+        this.idDokter = idDokter;
+    }
+
+    public String getNamaDokter() {
+        return namaDokter;
+    }
+
+    public void setNamaDokter(String namaDokter) {
+        this.namaDokter = namaDokter;
+    }
+
+    public String getSpesialis() {
+        return spesialis;
+    }
+
+    public void setSpesialis(String spesialis) {
+        this.spesialis = spesialis;
+    }
+
+    public String getNoTelp() {
+        return noTelp;
+    }
+
+    public void setNoTelp(String noTelp) {
+        this.noTelp = noTelp;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
