1b510a59bbf98cd4341da776afe3da5389cfeed2
Juwew1 <fauzyjunior53@gmail.com>
2025-12-17 07:47:47 +0700

    Bug Fixed

--- SistemReservasiKlinik/src/main/java/id/ac/ifunjani/sistemreservasiklinik/service/StaffService.java
+++ SistemReservasiKlinik/src/main/java/id/ac/ifunjani/sistemreservasiklinik/service/StaffService.java
@@ -1,4 +1,17 @@
 package id.ac.ifunjani.sistemreservasiklinik.service;
 
-public class StaffService {
+import id.ac.ifunjani.sistemreservasiklinik.model.Dokter;
+import id.ac.ifunjani.sistemreservasiklinik.model.Pasien;
+import id.ac.ifunjani.sistemreservasiklinik.model.Reservasi;
+import id.ac.ifunjani.sistemreservasiklinik.model.Staff;
+import java.util.List;
+
+public interface StaffService {
+    Staff getById(String idStaff);
+    long countPasien();
+    long countDokter();
+    long countReservasi();
+    List<Reservasi> getAllReservasi();
+    List<Pasien> getAllPasien();
+    List<Dokter> getAllDokter();
 }
