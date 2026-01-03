61c42def3d8bbd10a593fbc5715714d8ac5e707c
Juwew1 <fauzyjunior53@gmail.com>
2026-01-03 20:58:44 +0700

    menambahkan code logic untuk StaffRepoImpl

--- SistemReservasiKlinik/src/main/java/id/ac/ifunjani/sistemreservasiklinik/repository/StaffRepositoryImpl.java
+++ SistemReservasiKlinik/src/main/java/id/ac/ifunjani/sistemreservasiklinik/repository/StaffRepositoryImpl.java
@@ -1,16 +1,27 @@
 package id.ac.ifunjani.sistemreservasiklinik.repository;
 
 import id.ac.ifunjani.sistemreservasiklinik.model.Staff;
+import java.util.ArrayList;
+import java.util.List;
 
 public class StaffRepositoryImpl implements StaffRepository {
+    private List<Staff> dbStaff = new ArrayList<>();
+
+    public StaffRepositoryImpl() {
+        dbStaff.add(new Staff("ST001", "Fauzy Faadillah", "admin123"));
+        dbStaff.add(new Staff("ST002", "Admin Klinik", "pass321"));
+    }
 
     @Override
     public Staff findById(String idStaff) {
-        return null;
+        return dbStaff.stream()
+                .filter(s -> s.getIdStaff().equals(idStaff))
+                .findFirst()
+                .orElse(null);
     }
 
     @Override
     public long count() {
-        return 0;
+        return dbStaff.size();
     }
 }
