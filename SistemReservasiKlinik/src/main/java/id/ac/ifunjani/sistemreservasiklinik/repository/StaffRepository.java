1e16ed66fd01f6c5b2e381d5cc73f3bd03efc2ee
Juwew1 <fauzyjunior53@gmail.com>
2026-01-07 13:28:40 +0700

    Perbaikan code

--- SistemReservasiKlinik/src/main/java/id/ac/ifunjani/sistemreservasiklinik/repository/StaffRepository.java
+++ SistemReservasiKlinik/src/main/java/id/ac/ifunjani/sistemreservasiklinik/repository/StaffRepository.java
@@ -1,10 +1,25 @@
 package id.ac.ifunjani.sistemreservasiklinik.repository;
 
 import id.ac.ifunjani.sistemreservasiklinik.model.Staff;
+import java.util.ArrayList;
+import java.util.List;
 
-public interface StaffRepository {
+public class StaffRepository {
+    private List<Staff> dbStaff = new ArrayList<>();
 
-    Staff findById(String idStaff);
+    public StaffRepository() {
+        dbStaff.add(new Staff("ST001", "Fauzy Faadillah", "admin123"));
+        dbStaff.add(new Staff("ST002", "Admin Klinik", "pass321"));
+    }
 
-    long count();
+    public Staff findById(String idStaff) {
+        return dbStaff.stream()
+                .filter(s -> s.getIdStaff().equals(idStaff))
+                .findFirst()
+                .orElse(null);
+    }
+
+    public long count() {
+        return dbStaff.size();
+    }
 }
