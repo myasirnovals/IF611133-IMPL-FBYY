928973e644df87a53095072272dbd0162bcd25b2
MYN2997 <hawkinsdamien007@gmail.com>
2026-01-07 15:55:42 +0700

    perbaikan kode di pasien dashboard controller

--- SistemReservasiKlinik/src/main/java/id/ac/ifunjani/sistemreservasiklinik/controller/PasienDashboardController.java
+++ SistemReservasiKlinik/src/main/java/id/ac/ifunjani/sistemreservasiklinik/controller/PasienDashboardController.java
@@ -2,77 +2,115 @@ package id.ac.ifunjani.sistemreservasiklinik.controller;
 
 import id.ac.ifunjani.sistemreservasiklinik.model.Pasien;
 import id.ac.ifunjani.sistemreservasiklinik.model.RekamMedis;
-import id.ac.ifunjani.sistemreservasiklinik.repository.RekamMedisRepository;
+import id.ac.ifunjani.sistemreservasiklinik.model.Reservasi;
 import id.ac.ifunjani.sistemreservasiklinik.util.SceneManager;
 import id.ac.ifunjani.sistemreservasiklinik.util.UserSession;
 
-import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
-
-import java.time.LocalDate;
-import java.util.List;
 
 public class PasienDashboardController {
 
     @FXML
     private Label welcomeLabel;
+    @FXML
+    private Label idPasienLabel;
+    @FXML
+    private Label namaPasienLabel;
+    @FXML
+    private Label infoLabel;
+    @FXML
+    private TextField searchField;
 
     @FXML
-    private TableView<RekamMedis> rekamMedisTable;
+    private TableView<Reservasi> reservasiTable;
+    @FXML
+    private TableColumn<Reservasi, String> colIdReservasi;
+    @FXML
+    private TableColumn<Reservasi, String> colTanggal;
+    @FXML
+    private TableColumn<Reservasi, String> colDokter;
+    @FXML
+    private TableColumn<Reservasi, String> colPoli;
+    @FXML
+    private TableColumn<Reservasi, String> colStatus;
 
     @FXML
-    private TableColumn<RekamMedis, String> colTanggal;
-
+    private TableView<RekamMedis> rekamTable;
+    @FXML
+    private TableColumn<RekamMedis, String> colIdRekam;
+    @FXML
+    private TableColumn<RekamMedis, String> colTglPeriksa;
     @FXML
     private TableColumn<RekamMedis, String> colKeluhan;
-
     @FXML
     private TableColumn<RekamMedis, String> colDiagnosa;
 
-    @FXML
-    private TableColumn<RekamMedis, String> colDokter;
-
-    private RekamMedisRepository rekamMedisRepository = new RekamMedisRepository();
-    private Pasien currentPasien;
+    public PasienDashboardController() {
+    }
 
     @FXML
     public void initialize() {
-        Pasien pasienAktif = UserSession.getInstance().getPasien();
+        Pasien pasien = UserSession.getInstance().getPasien();
 
-        if (pasienAktif != null) {
-            this.currentPasien = pasienAktif;
-            welcomeLabel.setText("Selamat datang, Sdr/i " + pasienAktif.getNamaLengkap());
-
-            setupColumns();
-            loadData(pasienAktif.getIdPasien());
-        } else {
-            welcomeLabel.setText("Error: User belum login");
+        if (pasien == null) {
+            SceneManager.getInstance().switchScene("/id/ac/ifunjani/sistemreservasiklinik/view/login.fxml", "KlinikSehat Login");
+            return;
         }
+
+        welcomeLabel.setText("Selamat datang, " + pasien.getNamaLengkap());
+        idPasienLabel.setText(pasien.getIdPasien());
+        namaPasienLabel.setText(pasien.getNamaLengkap());
+
+        setupColumns();
+        loadData();
     }
 
     private void setupColumns() {
-        colTanggal.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getTanggalPeriksa().toString()));
+        colIdReservasi.setCellValueFactory(new PropertyValueFactory<>("idReservasi"));
+        colTanggal.setCellValueFactory(new PropertyValueFactory<>("tanggal"));
+        colDokter.setCellValueFactory(new PropertyValueFactory<>("namaDokter"));
+        colIdRekam.setCellValueFactory(new PropertyValueFactory<>("idRekamMedis"));
+        colTglPeriksa.setCellValueFactory(new PropertyValueFactory<>("tanggalPeriksa"));
         colKeluhan.setCellValueFactory(new PropertyValueFactory<>("keluhan"));
         colDiagnosa.setCellValueFactory(new PropertyValueFactory<>("diagnosa"));
-        colDokter.setCellValueFactory(new PropertyValueFactory<>("idDokter"));
     }
 
-    private void loadData(String idPasien) {
-        List<RekamMedis> listRekamMedis = rekamMedisRepository.findByPasienId(idPasien);
-        ObservableList<RekamMedis> data = FXCollections.observableArrayList(listRekamMedis);
-
-        rekamMedisTable.setItems(data);
+    private void loadData() {
+        infoLabel.setText("Data berhasil dimuat (Dummy/Kosong).");
     }
 
     @FXML
-    private void handleLogout() {
+    public void onRefresh() {
+        loadData();
+        infoLabel.setText("Data diperbarui.");
+    }
+
+    @FXML
+    public void onBuatReservasi() {
+        System.out.println("Tombol Buat Reservasi ditekan");
+    }
+
+    @FXML
+    public void onDetailReservasi() {
+        System.out.println("Tombol Detail Reservasi ditekan");
+    }
+
+    @FXML
+    public void onBatalkanReservasi() {
+        System.out.println("Tombol Batalkan Reservasi ditekan");
+    }
+
+    @FXML
+    public void onDetailRekamMedis() {
+        System.out.println("Tombol Detail Rekam Medis ditekan");
+    }
+
+    @FXML
+    public void onLogout() {
         UserSession.getInstance().clearSession();
-        SceneManager.getInstance().switchScene("/id/ac/ifunjani/sistemreservasiklinik/view/login.fxml", "Login Klinik Sehat");
+        SceneManager.getInstance().switchScene("/id/ac/ifunjani/sistemreservasiklinik/view/login.fxml", "KlinikSehat Login");
     }
 }
